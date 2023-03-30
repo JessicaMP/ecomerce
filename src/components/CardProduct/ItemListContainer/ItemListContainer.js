f6a339730b6393d03efbbc2c867bb59f860f212f
Jessica <jessica95.mp@gmail.com>
2023-03-29 20:46:27 -0500

    mostrar mensaje en caso no tenga productos en alguna categoria

--- src/components/CardProduct/ItemListContainer/ItemListContainer.js
+++ src/components/CardProduct/ItemListContainer/ItemListContainer.js
@@ -1,34 +1,47 @@
 import ItemList from "../ItemList/ItemList";
-import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { getProducts, getProductsByCategory } from "../../../assets/data/mock"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getProducts, getProductsByCategory } from "../../../assets/data/mock";
 
+const NotContent = ({label}) => {
+  return (
+    <div className="flex justify-center items-center p-10">
+    <p className="font-medium text-base">{label}</p>
+    </div>
+  )
+}
 
 const ItemListContainer = ({ greeting }) => {
-  const [products, setProducts] = useState([])
-  const [loading, setLoading] = useState(true)
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const { categoryId } = useParams()
+  const { categoryId } = useParams();
 
-    useEffect(() => {
-        const asyncFunction = categoryId ? getProductsByCategory : getProducts
-
-        asyncFunction(categoryId)
-            .then(products => {
-                setProducts(products)
-            })
-            .catch(error => {
-                console.log(error)
-            }).finally(()=> {
-              setLoading(false)
-          })
-    }, [categoryId])
+  useEffect(() => {
+    const asyncFunction = categoryId ? getProductsByCategory : getProducts;
+    setLoading(true);
+    asyncFunction(categoryId)
+      .then((products) => {
+        setProducts(products);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [categoryId]);
 
   return (
     <div className="container mx-auto py-4">
       <h1 className="text-center text-black font-bold text-xl"> {greeting}</h1>
-      {loading? <p>Cargando...</p>: <ItemList products={products}/>}
-      
+      {loading ? (
+        <p>Cargando...</p>
+      ) : products.length > 0 ? (
+        <ItemList products={products} />
+      ) : (
+       <NotContent label={`No hay productos en la categoria ${categoryId}`}/>
+      )}
     </div>
   );
 };
